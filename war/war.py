989d5577de9d8c38bfb5f11bf4ea672684268384
Emily Strickland <emily@zubon.org>
2013-04-30 19:35:46 -0700

    Adhere closer to both the rules of War and to recursion.

--- war/war.py
+++ war/war.py
@@ -33,39 +33,49 @@ class War:
 
         while True:
             self.pot = []
-            next_battle = {}
+            battle = {}
             for player in self.players.itervalues():
                 if len(player.hand) > 0:
-                    next_battle[id(player)] = player.play_card()
-            self.battle(next_battle)
+                    battle[id(player)] = player.play_card()
+            battle_winner = self.battle(battle)
+            battle_winner.take_cards(self.pot)
 
             # print the outcome
-            winner = self.get_winner()
-            if winner:
-                return winner
+            war_winner = self.get_winner()
+            if war_winner:
+                return war_winner
             elif verbose:
                 status = ''
                 for player in self.players.itervalues():
                     status += '*' * len(player.hand) + ' '
                 print status
 
-    def battle(self, current_battle={}):
-        high_card = max(current_battle.values(), key=lambda c: c.rank)
+    def battle(self, battle={}):
+        """
+        Accepts a hash of players with the cards they play. Recursively
+        calls itself until there is a single definitive winner, which
+        it returns. Accumulates cards played in the pot for each call.
+        """
 
+        high_card = max(battle.values(), key=lambda c: c.rank)
         battle_winners = []
-        for player_id, card in current_battle.iteritems():
+        for player_id, card in battle.iteritems():
             if card == high_card:
                 battle_winners.append(self.players[player_id])
 
-        self.pot.extend(current_battle.values())
+        self.pot.extend(battle.values())
+
         if len(battle_winners) == 1:
-            battle_winners[0].take_cards(self.pot)
+            return battle_winners[0]
         else:
             next_battle = {}
             for player in battle_winners:
                 if len(player.hand) > 0:
+                    # another card face down ...
+                    self.pot.extend(player.play_card())
+                    # ... and another card face up
                     next_battle[id(player)] = player.play_card()
-            self.battle(next_battle)
+            return self.battle(next_battle)
 
     def get_winner(self):
         """
