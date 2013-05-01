e61c36c63de056396c7d47723c7c98895542e4df
Emily Strickland <emily@zubon.org>
2013-04-30 21:11:44 -0700

    More test stubs.

---
+++ war/tests/test_player.py
@@ -0,0 +1,10 @@
+import unittest
+
+from war.player import Player
+
+
+class TestPlayer(unittest.TestCase):
+    def test_player(self):
+        pass
+
+    # TODO: test for init cases which raise exceptions
--- war/tests/test_war.py
+++ war/tests/test_war.py
@@ -17,3 +17,6 @@ class TestWar(unittest.TestCase):
     def test_war_plays_game_without_failing_and_returns_a_winner(self):
         w = War()
         assert isinstance(w.play(), Player)
+
+    # TODO: test for infinite recursion edge cases in War.battle()
+    # TODO: test for init cases which should raise exceptions
